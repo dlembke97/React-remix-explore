b45bc8650b90a28892f9cba7594b7d463deeae39
dlembke3 <dlembke3@gatech.edu>
2025-08-24 16:34:43 -0500

    push

--- app/routes/home.tsx
+++
@@ -1,6 +0,0 @@
-export const meta = () => [
-  { title: 'New Remix App' },
-  { name: 'description', content: 'Welcome to Remix with Ant Design!' },
-];
-
-export { default } from '../components/Home';
