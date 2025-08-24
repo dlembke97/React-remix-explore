0391f1e21ed586f454da0f9e55db78550fd844d2
dlembke97 <101201424+dlembke97@users.noreply.github.com>
2025-08-24 13:43:16 -0500

    chore: remove favicon asset

---
+++ app/routes/home.tsx
@@ -0,0 +1,6 @@
+export const meta = () => [
+  { title: 'New Remix App' },
+  { name: 'description', content: 'Welcome to Remix with Ant Design!' },
+];
+
+export { default } from '../components/Home';
